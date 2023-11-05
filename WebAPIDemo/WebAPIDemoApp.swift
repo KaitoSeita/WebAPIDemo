b790f275c78d1a6cc4cf7d4e60d12084e5fff255
kaitoseita <leadik2468@gmail.com>
2023-11-05 14:55:22 +0900

    Initial Commit

---
+++ WebAPIDemo/WebAPIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebAPIDemoApp.swift
+//  WebAPIDemo
+//
+//  Created by セイタカイト on 2023/11/05.
+//
+
+import SwiftUI
+
+@main
+struct WebAPIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
