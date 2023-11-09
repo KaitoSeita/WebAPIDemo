1be8d6df0c24de60647679ccb918d5199a76036b
kaitoseita <leadik2468@gmail.com>
2023-11-09 15:16:11 +0900

    ArticleのモデルとViewでつかうArticleListの定義

--- WebAPIDemo/Model/Article.swift
+++ WebAPIDemo/Model/Article.swift
@@ -5,11 +5,24 @@
 //  Created by kaito-seita on 2023/11/05.
 //
 
-struct Article: Codable, Hashable {
+import SwiftUI
+
+struct Article: Codable {
     let comments_count: Int?
     let created_at: String?
     let likes_count: Int?
-    let title: String?
-    let url: String?
+    let title: String
+    let url: String
     let page_views_count: Int?
 }
+
+struct ArticleList: Identifiable {
+    let id = UUID()
+    let commentsCount: Int
+    let createdAt: String
+    let likesCount: Int
+    let title: String
+    let url: String
+    let viewCount: Int
+}
+// title, urlはオプショナルでない
